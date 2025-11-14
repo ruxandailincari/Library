85d7b86935ea15e9f106141168b9d1bcb894f031
Ruxanda Ilincari <ruxanda.ilincari@gmail.com>
2025-11-14 19:39:00 +0200

    Added Notification pattern support for user in service, repository and controller done

--- src/main/java/repository/user/UserRepositoryMySQL.java
+++ src/main/java/repository/user/UserRepositoryMySQL.java
@@ -2,6 +2,7 @@ package repository.user;
 
 import model.User;
 import model.builder.UserBuilder;
+import model.validator.Notification;
 import repository.security.RightsRolesRepository;
 
 import java.sql.*;
@@ -25,25 +26,32 @@ public class UserRepositoryMySQL implements UserRepository{
     }
 
     @Override
-    public User findByUsernameAndPassword(String username, String password) {
+    public Notification<User> findByUsernameAndPassword(String username, String password) {
+        Notification<User> findByUsernameAndPasswordNotification = new Notification<>();
         try {
             String fetchUserSql = "Select * from `" + USER + "` where username=? and password=?;";
             PreparedStatement preparedStatement = connection.prepareStatement(fetchUserSql);
             preparedStatement.setString(1, username);
             preparedStatement.setString(2, password);
             ResultSet userResultSet = preparedStatement.executeQuery();
-            userResultSet.next();
+            if (userResultSet.next()){
 
-            User user = new UserBuilder()
-                    .setUsername(userResultSet.getString("username"))
-                    .setPassword(userResultSet.getString("password"))
-                    .setRoles(rightsRolesRepository.findRolesForUser(userResultSet.getLong("id")))
-                    .build();
-            return user;
+                User user = new UserBuilder()
+                        .setUsername(userResultSet.getString("username"))
+                        .setPassword(userResultSet.getString("password"))
+                        .setRoles(rightsRolesRepository.findRolesForUser(userResultSet.getLong("id")))
+                        .build();
+                findByUsernameAndPasswordNotification.setResult(user);
+            } else {
+                findByUsernameAndPasswordNotification.addError("Invalid username or password!");
+                return findByUsernameAndPasswordNotification;
+            }
         } catch (SQLException e){
             System.out.println(e.toString());
+            findByUsernameAndPasswordNotification.addError("Something is wrong with the Database!");
+
         }
-        return null;
+        return findByUsernameAndPasswordNotification;
     }
 
     @Override
