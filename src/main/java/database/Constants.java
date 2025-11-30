6a4b08379a95f143b37edd82c8d429368e0323dd
Ruxanda Ilincari <ruxanda.ilincari@gmail.com>
2025-11-30 23:27:23 +0200

    Added order table in the SQLTableCreation/Fixed bootstrap so that price and stock for the book table are created in the SQLTableCreationFactory

--- src/main/java/database/Constants.java
+++ src/main/java/database/Constants.java
@@ -39,9 +39,10 @@ public class Constants {
         public static final String RIGHT = "right";
         public static final String ROLE_RIGHT = "role_right";
         public static final String USER_ROLE = "user_role";
+        public static final String ORDER = "order";
 
         public static final String[] ORDERED_TABLES_FOR_CREATION =
-                new String[]{USER, ROLE, RIGHT, ROLE_RIGHT, USER_ROLE, BOOK};
+                new String[]{USER, ROLE, RIGHT, ROLE_RIGHT, USER_ROLE, BOOK, ORDER};
     }
 
     public static class Roles {
